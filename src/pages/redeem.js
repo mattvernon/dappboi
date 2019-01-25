c080b3662115d6b968874c943c8bff02b5aa0ed0
mattvernon <hello@matthewvernon.co>
2019-01-25 13:27:11 +1100

    Update redeem.js

--- src/pages/redeem.js
+++ src/pages/redeem.js
@@ -6,12 +6,22 @@ const Redeem = () => {
   return (
     <div className="page">
       <h1>Redeem BOI</h1>
-      <p className="step-one"><span>1.</span> Send your BOI back to this exact address:</p>
+      <p className="step-one">
+        <span>1.</span> Send your BOI back to this exact address:
+      </p>
       <div className="address-field">0xB18A83EEa77Db549333eAA9E964d8887517B7C59</div>
-      <img src={qr} className="qr" />
-      <p className="small-text">Please note, 1.0 BOI is redeemable for 1.0 hour of design work. Multiple BOI are redeemable for more than one hour of work, but I cannot accept fragments. Please ensure you only send whole BOI. </p>
-      <p className="warning-text">Please for the love of god, do not send Bitcoin, or any other type of digital currency to this address. ONLY BOI!</p>
-      <p className="step-two"><span>2.</span> Email me at hello@matthewvernon.co to organise the project.</p>
+      <p className="small-text">
+        Please note, 1.0 BOI is redeemable for 1.0 hour of design work. Multiple BOI are redeemable
+        for more than one hour of work, but I cannot accept fragments. Please ensure you only send
+        whole BOI.{' '}
+      </p>
+      <p className="warning-text">
+        Please for the love of god, do not send Bitcoin, or any other type of digital currency to
+        this address. ONLY BOI!
+      </p>
+      <p className="step-two">
+        <span>2.</span> Email me at hello@matthewvernon.co to organise the project.
+      </p>
     </div>
   );
 };

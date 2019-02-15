1ed1dc70e929c11a306405186fc6a8cf0a5e95bc
mattvernon <hello@matthewvernon.co>
2019-02-15 11:41:05 -0800

    updated signedOrder

--- src/components/Instant.js
+++ src/components/Instant.js
@@ -27,17 +27,17 @@ class Instant extends React.Component {
           takerAddress: '0x0000000000000000000000000000000000000000',
           makerFee: new BigNumber('0'),
           takerFee: new BigNumber('0'),
-          makerAssetAmount: new BigNumber('25000000000000000000'),
-          takerAssetAmount: new BigNumber('16250000000000000000'),
+          makerAssetAmount: new BigNumber('20000000000000000000'),
+          takerAssetAmount: new BigNumber('13000000000000000000'),
           makerAssetData:
             '0xf47261b0000000000000000000000000439ce375e3ee4dc203d71958beca3c0f417d65cb',
           takerAssetData:
             '0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-          expirationTimeSeconds: new BigNumber('1549795920'),
+          expirationTimeSeconds: new BigNumber('1551209820'),
           feeRecipientAddress: '0x0000000000000000000000000000000000000000',
-          salt: '93115335222072847214375889428160398014420090087211579678016219042290848222197',
+          salt: '75133064172400108999414834901383488139185620257556898178132978563020169124698',
           signature:
-            '0x1c6ca33d0d54f5696951a698d87de5e925dc0c3241e414bc0ac5e6103adab512341a4adbac779cf56cd35bbd071d42ae50ad7f4e3eebe5c9b72b59994a2def571303',
+            '0x1b12c730418af154ece4887d125a6932d94ed04e9401a6c5651a247ea0705017cd358c27bad45d3d9d182b3eb462b6d78781674b0c50479ef32737b2d84d48006003',
           exchangeAddress: '0x4f833a24e1f95d70f028921e27040ca56e09ab0b'
         }
       ],
